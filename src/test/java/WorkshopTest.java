ccb3b935d95b6f9f60d3ec6a7578a7417926a23f
sigmotoa <si.galvismotoa@gmail.com>
2024-09-09 17:11:12 -0500

    Fixing pom

--- src/test/java/WorkshopTest.java
+++ src/test/java/WorkshopTest.java
@@ -8,8 +8,8 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 //import JUNIT 4
 
 
